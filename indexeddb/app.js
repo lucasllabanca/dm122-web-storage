568c4bec07124199fc3eebdc6514b7d91dc0f5ce
lucasllabanca <lucasllabanca@hotmail.com>
2022-10-01 16:43:06 -0300

    feat: created dynamic html

--- indexeddb/app.js
+++ indexeddb/app.js
@@ -18,5 +18,32 @@ db.on('populate', async () => {
 
 db.open();
 
-const pokemon = await db.pokemon.toArray();
-console.log(pokemon);
+// function byChar(char) {
+//     return function (poke) {
+//         return poke.name.includes(char);
+//     }
+// }
+
+//const byChar = (char) => (poke) => poke.name.includes(char);
+//const pokemon = await db.pokemon.filter(byChar('a')).toArray();
+//const pokemon = await db.pokemon.where('name').startsWithIgnoreCase('a').toArray();
+//console.log(pokemon);
+
+const pokemonList = await db.pokemon.toArray();
+
+const pokeHTML = pokemonList.map(toHTML).join('');
+document.body.innerHTML = pokeHTML;
+
+function toHTML(poke) {
+    return `
+        <div class="card grass">
+            <header>
+                <h2>#${poke.id}</h2>
+            </header>
+            <img src="${poke.picture}" alt="${poke.name}" title="${poke.name}">
+            <footer class="grass">
+            <span>${poke.name}</span>
+            </footer>
+        </div>
+    `;
+}
