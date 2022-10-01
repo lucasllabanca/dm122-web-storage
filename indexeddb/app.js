c9da01b4cfe21f84b95eb0e2616278f8ccbb6ddf
lucasllabanca <lucasllabanca@hotmail.com>
2022-10-01 15:39:28 -0300

    refactor: removed modules

--- indexeddb/app.js
+++ indexeddb/app.js
@@ -1,4 +1,1 @@
-import { MyFirstModule, MySecondModule } from "./myFirstModule";
-
-MyFirstModule('Hi there!')
-MySecondModule('Second Module')
+import Dexie from "https://cdn.jsdelivr.net/npm/dexie@3.0.3/dist/dexie.mjs";
