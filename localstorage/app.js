5abec5459f15bca4aa56d96c4b5533d717042744
lucasllabanca <lucasllabanca@hotmail.com>
2022-10-01 11:42:39 -0300

    feat: check form data

--- localstorage/app.js
+++ localstorage/app.js
@@ -2,5 +2,14 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    alert(`Submitted successfully`);
+    //console.log({ event });
+    const form = event.target; //who triggered the event
+
+    const formData = {
+        key: form.key.value,
+        value: form.value.value
+    }
+
+    //console.log({ formData });
+    
 });
