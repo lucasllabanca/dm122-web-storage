904744ea3042b8cb3dcd4f2132492eaf0459255b
lucasllabanca <lucasllabanca@hotmail.com>
2022-10-08 10:59:21 -0300

    feat: fetch and show pokemon

--- indexeddb/app.js
+++ indexeddb/app.js
@@ -3,16 +3,16 @@ import Dexie from "https://cdn.jsdelivr.net/npm/dexie@3.0.3/dist/dexie.mjs";
 const db = new Dexie('pokemonDB');
 
 db.version(1).stores({
-    pokemon: '++id, name'
+    pokemon: '++id, number, name'
 });
 
 db.on('populate', async () => {
     //Top-level await if out of async func, cause of module
     await db.pokemon.bulkPut([
-        { name: 'Bulbasaur', picture: await downloadImage(buildUrl(1)) },
-        { name: 'Charmander', picture: await downloadImage(buildUrl(4)) },
-        { name: 'Squirtle', picture: await downloadImage(buildUrl(7)) },
-        { name: 'Pikachu', picture: await downloadImage(buildUrl(25)) }
+        { name: 'Bulbasaur', number: 1, picture: await downloadImage(buildUrl(1)) },
+        { name: 'Charmander', number: 4, picture: await downloadImage(buildUrl(4)) },
+        { name: 'Squirtle', number: 7, picture: await downloadImage(buildUrl(7)) },
+        { name: 'Pikachu', number: 25, picture: await downloadImage(buildUrl(25)) }
     ]);
 });
 
@@ -44,7 +44,7 @@ async function retrieveData() {
         return `
             <div class="card grass">
                 <header>
-                    <h2>#${poke.id}</h2>
+                    <h2>#${poke.number}</h2>
                 </header>
                 <img src="${URL.createObjectURL(poke.picture)}" alt="${poke.name}" title="${poke.name}">
                 <footer class="grass">
@@ -80,6 +80,7 @@ async function saveOnDb({ name, pokeNumber }) {
     if (pokemon.length === 0) {
         await db.pokemon.add({
             name,
+            number: pokeNumber,
             picture: await downloadImage(await buildUrl(pokeNumber))
         });
     }
